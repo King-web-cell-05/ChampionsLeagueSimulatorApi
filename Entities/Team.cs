d21bea3edb15843ee38ddbf7458b2f377354fc5c
Dada Kingsley <Kingsleydada159@gmail.com>
2026-02-16 12:28:05 -0800

    Created team entity

---
+++ Entities/Team.cs
@@ -0,0 +1,12 @@
+﻿namespace ChampionsLeagueSimulatorAPI.Entities;
+
+public class Team
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Country { get; set; } = string.Empty;
+
+    public int Coefficient { get; set; }
+}
