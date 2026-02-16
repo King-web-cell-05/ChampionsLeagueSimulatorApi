e286eb8dbe8dec88f571dabd5da23b23d140a062
Dada Kingsley <Kingsleydada159@gmail.com>
2026-02-16 12:59:31 -0800

    Added dbcontext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ChampionsLeagueSimulatorAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace ChampionsLeagueSimulatorAPI.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Team> Teams => Set<Team>();
+}
