167549b6f9f67736e2644a94007048d4ca0c8fc9
Dada Kingsley <Kingsleydada159@gmail.com>
2026-02-16 23:35:04 -0800

    Created controllers

---
+++ Controllers/TeamController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ChampionsLeagueSimulatorAPI.Data;
+using ChampionsLeagueSimulatorAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChampionsLeagueSimulatorAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TeamsController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public TeamsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateTeam(Team team)
+    {
+        _context.Teams.Add(team);
+        await _context.SaveChangesAsync();
+
+        return Ok(team);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetTeams()
+    {
+        return Ok(await _context.Teams.ToListAsync());
+    }
+}
