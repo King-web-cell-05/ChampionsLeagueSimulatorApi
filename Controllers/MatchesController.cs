0c90dc05d229d2461c6fba1f30af8efc1c6204f1
Dada Kingsley <Kingsleydada159@gmail.com>
2026-02-19 22:19:45 -0800

    Created matchContoller

---
+++ Controllers/MatchesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ChampionsLeagueSimulatorAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChampionsLeagueSimulatorAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MatchesController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public MatchesController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost("{matchId}/result")]
+    public async Task<IActionResult> AddResult(Guid matchId, int homeScore, int awayScore)
+    {
+        var match = await _context.Matches.FindAsync(matchId);
+
+        if (match == null)
+            return NotFound();
+
+        match.HomeScore = homeScore;
+        match.AwayScore = awayScore;
+        match.IsPlayed = true;
+
+        await _context.SaveChangesAsync();
+
+        return Ok(match);
+    }
+}
