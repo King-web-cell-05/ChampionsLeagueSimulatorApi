472372349fdf3073645caf16d4d46fa1dffd131c
Dada Kingsley <Kingsleydada159@gmail.com>
2026-02-20 11:43:47 -0800

    Add teamrequest.cs

---
+++ Controllers/CompetitionTeamsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ChampionsLeagueSimulatorAPI.Data;
+using ChampionsLeagueSimulatorAPI.Entities;
+using ChampionsLeagueSimulatorAPI.DTOs;
+
+namespace ChampionsLeagueSimulatorAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CompetitionTeamsController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public CompetitionTeamsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+
+    // ADD THIS METHOD HERE
+    [HttpPost]
+    public async Task<IActionResult> AddTeam(AddCompetitionTeamRequest request)
+    {
+        var entry = new CompetitionTeam
+        {
+            CompetitionId = request.CompetitionId,
+            TeamId = request.TeamId
+        };
+
+        _context.CompetitionTeams.Add(entry);
+
+        await _context.SaveChangesAsync();
+
+        return Ok(entry);
+    }
+
+}
---
+++ DTOs/AddCompetitionTeamRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace ChampionsLeagueSimulatorAPI.DTOs;
+
+public class AddCompetitionTeamRequest
+{
+    public Guid CompetitionId { get; set; }
+
+    public Guid TeamId { get; set; }
+}
