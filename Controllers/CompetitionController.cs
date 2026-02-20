353ed10a2e1a7b3c4ca79d7f856b7ca1e74a053d
Dada Kingsley <Kingsleydada159@gmail.com>
2026-02-19 22:17:55 -0800

    Created competition controlleer

---
+++ Controllers/CompetitionController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ChampionsLeagueSimulatorAPI.Data;
+using ChampionsLeagueSimulatorAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChampionsLeagueSimulatorAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CompetitionController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public CompetitionController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateCompetition(Competition competition)
+    {
+        _context.Competitions.Add(competition);
+
+        await _context.SaveChangesAsync();
+
+        return Ok(competition);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetCompetitions()
+    {
+        return Ok(await _context.Competitions.ToListAsync());
+    }
+}
