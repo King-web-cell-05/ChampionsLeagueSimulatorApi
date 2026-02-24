a4a8b831f4ad93f2c98bb5528680dc848abed4c4
Dada Kingsley <Kingsleydada159@gmail.com>
2026-02-23 23:37:05 -0800

    Added migration for match simualtion fix

---
+++ Migrations/20260224072220_FixMatchRelationships.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChampionsLeagueSimulatorApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixMatchRelationships : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Matches_Competitions_CompetitionId",
+                table: "Matches");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Matches_Teams_AwayTeamId",
+                table: "Matches");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Matches_Teams_HomeTeamId",
+                table: "Matches");
+
+            migrationBuilder.DropTable(
+                name: "Standings");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CompetitionId1",
+                table: "Matches",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matches_CompetitionId1",
+                table: "Matches",
+                column: "CompetitionId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Matches_Competitions_CompetitionId",
+                table: "Matches",
+                column: "CompetitionId",
+                principalTable: "Competitions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Matches_Competitions_CompetitionId1",
+                table: "Matches",
+                column: "CompetitionId1",
+                principalTable: "Competitions",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Matches_Teams_AwayTeamId",
+                table: "Matches",
+                column: "AwayTeamId",
+                principalTable: "Teams",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Matches_Teams_HomeTeamId",
+                table: "Matches",
+                column: "HomeTeamId",
+                principalTable: "Teams",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Matches_Competitions_CompetitionId",
+                table: "Matches");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Matches_Competitions_CompetitionId1",
+                table: "Matches");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Matches_Teams_AwayTeamId",
+                table: "Matches");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Matches_Teams_HomeTeamId",
+                table: "Matches");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Matches_CompetitionId1",
+                table: "Matches");
+
+            migrationBuilder.DropColumn(
+                name: "CompetitionId1",
+                table: "Matches");
+
+            migrationBuilder.CreateTable(
+                name: "Standings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TeamId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CompetitionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Draws = table.Column<int>(type: "int", nullable: false),
+                    GoalsAgainst = table.Column<int>(type: "int", nullable: false),
+                    GoalsFor = table.Column<int>(type: "int", nullable: false),
+                    Losses = table.Column<int>(type: "int", nullable: false),
+                    Played = table.Column<int>(type: "int", nullable: false),
+                    Points = table.Column<int>(type: "int", nullable: false),
+                    Wins = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Standings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Standings_Teams_TeamId",
+                        column: x => x.TeamId,
+                        principalTable: "Teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Standings_TeamId",
+                table: "Standings",
+                column: "TeamId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Matches_Competitions_CompetitionId",
+                table: "Matches",
+                column: "CompetitionId",
+                principalTable: "Competitions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Matches_Teams_AwayTeamId",
+                table: "Matches",
+                column: "AwayTeamId",
+                principalTable: "Teams",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Matches_Teams_HomeTeamId",
+                table: "Matches",
+                column: "HomeTeamId",
+                principalTable: "Teams",
+                principalColumn: "Id");
+        }
+    }
+}
