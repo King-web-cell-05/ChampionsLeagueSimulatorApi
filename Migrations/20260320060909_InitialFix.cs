a23f40e6c0ccee4c4bb5ed56cb9e42687765abca
Dada Kingsley <Kingsleydada159@gmail.com>
2026-03-19 23:11:06 -0700

    Added migration

---
+++ Migrations/20260320060909_InitialFix.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChampionsLeagueSimulatorApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CompetitionTeams",
+                table: "CompetitionTeams");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CompetitionTeams_CompetitionId",
+                table: "CompetitionTeams");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "CompetitionTeams");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CompetitionTeams",
+                table: "CompetitionTeams",
+                columns: new[] { "CompetitionId", "TeamId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CompetitionTeams",
+                table: "CompetitionTeams");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Id",
+                table: "CompetitionTeams",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CompetitionTeams",
+                table: "CompetitionTeams",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompetitionTeams_CompetitionId",
+                table: "CompetitionTeams",
+                column: "CompetitionId");
+        }
+    }
+}
